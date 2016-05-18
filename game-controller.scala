b03435b0449e12a5977be1f06a9fceaa0834cb7b
Robert Blankenship <rablanken13@gmail.com>
2016-05-17 21:09:54 -0700

    Play opening song on web client. Sending the movables via the websocket. Game can now support multiple connections at once. Live updates of the game state :). Movement for a remote player!! :)

--- game-controller.scala
+++ game-controller.scala
@@ -13,61 +13,53 @@
 package pacman
 
 import java.net.ServerSocket
-import java.io.PrintStream
+import java.io.{ PrintStream, InputStream, OutputStream }
 import io.BufferedSource
 
-object GameController extends App {
-  val socket = new ServerSocket(8000)
+object SocketUtil {
+  def handshake(inputStream: InputStream, outputStream: OutputStream) {
 
-  val connection = socket.accept()
-  
-  val inputStream = connection.getInputStream()
-  val outputStream = connection.getOutputStream()
- 
-  val inputSource = new BufferedSource(inputStream)
-  val inputLines = inputSource.getLines
+    def readHeaders(inputStream: InputStream): collection.mutable.Map[String, String] = {
+      val headers = collection.mutable.Map[String, String]()
 
-  val protocol = inputLines.next()
-  val headers = collection.mutable.Map[String, String]()
-  
-  var hasMore = true
+      val inputSource = new BufferedSource(inputStream)
+      val inputLines = inputSource.getLines.drop(0)
 
-  while (hasMore) {
-    val header = inputLines.next()
-    println(header)
-
-    if (header.length > 2) {
-      val headerName = header.split(":".charAt(0))(0)
-      val headerValue = header.split(" ".charAt(0))(1)
-   
-      headers(headerName) = headerValue
-    } else {
-      hasMore = false
+      var hasMore = true 
+      while (hasMore) {
+        val header = inputLines.next()
+        if (header.length > 2) {
+          val headerName = header.split(":".charAt(0))(0)
+          val headerValue = header.split(" ".charAt(0))(1)
+          headers(headerName) = headerValue
+        } else {
+          hasMore = false
+        }
+      }
+      headers
     }
-  }
+    
+    val webSocketAccept = {
+      val webSocketKey = readHeaders(inputStream)("Sec-WebSocket-Key")
 
-  val webSocketKey = headers("Sec-WebSocket-Key")
-  
-  val webSocketAccept = {
-    java.util.Base64.getEncoder.encodeToString {
-      java.security.MessageDigest
-        .getInstance("SHA-1")
-        .digest(s"${webSocketKey}258EAFA5-E914-47DA-95CA-C5AB0DC85B11".getBytes)
+      java.util.Base64.getEncoder.encodeToString {
+        java.security.MessageDigest
+          .getInstance("SHA-1")
+          .digest(s"${webSocketKey}258EAFA5-E914-47DA-95CA-C5AB0DC85B11".getBytes)
+      }
     }
+  
+    val out = new PrintStream(outputStream)
+
+    out.println("HTTP/1.1 101 Switching Protocols")
+    out.println("Upgrade: websocket")
+    out.println("Connection: Upgrade")
+    out.println(s"Sec-WebSocket-Accept: ${webSocketAccept}")
+    out.println("")
+    out.flush()
   }
-
-  val out = new PrintStream(outputStream)
-
-  out.println("HTTP/1.1 101 Switching Protocols")
-  out.println("Upgrade: websocket")
-  out.println("Connection: Upgrade")
-  out.println(s"Sec-WebSocket-Accept: ${webSocketAccept}")
-  out.println("")
-  out.flush()
-
-  inputStream.getClass.getMethods.foreach(println(_))
-
-  def sendMessage(stream: java.io.OutputStream, message: String) = {
+  
+  def sendMessage(stream: OutputStream, message: String) {
     stream.write(129.toByte)
 
     message.getBytes.length match {
@@ -75,28 +67,29 @@ object GameController extends App {
         stream.write(length.toByte)
       case length if length > 126 && length < 65536 =>
         stream.write(126.toByte)
-        println(s"length: $length")
         stream.write((length >> 8 & 255).toByte)
         stream.write((length >> 0 & 255).toByte)
       case length if length > 65536 =>
-        stream.write(127.toByte)
+        // TODO
+        stream.write(0.toByte)
     }
 
     stream.write(message.getBytes)
   }
 
-  def readMessage(stream: java.io.InputStream): String = {
-    val fin = stream.read()
+  def readMessage(stream: InputStream): String = {
+    val messageStatusCode = stream.read()
 
     val lengthByte = stream.read()
     
     val messageLength = {
       if (lengthByte - 128 < 125) {
         lengthByte - 128
-      // TODO: These cases are wrong.
       } else if (lengthByte - 128 == 126) {
+        // TODO
         stream.read() * stream.read()
       } else if (lengthByte - 128 == 127) {
+        // TODO
         for (i <- 1 to 8) stream.read()
       }
     }
@@ -105,20 +98,68 @@ object GameController extends App {
     val messageEncoded = for (i <- 1 to stream.available) yield stream.read()
     val messageDecoded = for (i <- 0 to messageEncoded.length - 1) yield (messageEncoded(i) ^ messageKey(i % 4))
 
-    println(s"Message length: $messageLength")
-    println(s"Message key: $messageKey")
-    println(s"Message encoded: $messageEncoded")
-    println(s"Message decoded: $messageDecoded")
     new String(messageDecoded.map(_.toByte).map(_.toChar).toArray)
-
   }
+}
 
-  
-  val game = new Game()
 
-  def sendMaze(stream: java.io.OutputStream, maze: Map[(Int, Int), DrawableElement]) {
-    var json = "{"
-    json += {
+object SocketServer extends App {
+  val socket = new ServerSocket(8000)
+
+  while (true) {
+    val connection = socket.accept()
+    println("Connection from new client received!")
+    
+    val inputStream = connection.getInputStream()
+    val outputStream = connection.getOutputStream()
+    
+    SocketUtil.handshake(inputStream, outputStream)
+   
+    val game = new Game()
+
+    // What kind of concurrency problems does this create for a client?
+    val inputThread = new Thread(new Runnable {
+      def run {
+        while (true) {
+          game.player.directionRequest = SocketUtil.readMessage(inputStream) match {
+            case "East" => East
+            case "West" => West
+            case "North" => North
+            case "South" => South
+            case (key: String)  =>
+              println(s"Recevied bad direction $key")
+              game.player.directionRequest
+          }
+        }
+      }
+    })
+    inputThread.start()
+
+    val outputThread = new Thread(new Runnable {
+      def run {
+        SocketUtil.sendMessage(outputStream, GameController.mazeToJson(game.maze))
+        SocketUtil.sendMessage(outputStream, GameController.movablesToJson(game.movables))
+
+        // Wait for the opening song.
+        Thread.sleep(4000)
+        
+        game.start()
+
+        while (true) {
+          Thread.sleep(20)
+          SocketUtil.sendMessage(outputStream, GameController.mazeToJson(game.maze))
+          SocketUtil.sendMessage(outputStream, GameController.movablesToJson(game.movables))
+        }
+      }
+    })
+    outputThread.start()
+  }
+}
+
+object GameController {
+  // TODO: Can we make this even cleaner using only Java/Scala built-ins?
+  def mazeToJson(maze: Map[(Int, Int), DrawableElement]): String = {
+    "{" + {
       "\"maze\":" + "[" + maze.map {
         case ((column: Int, row: Int), skin: Space) =>
           "{" + 
@@ -134,30 +175,26 @@ object GameController extends App {
             "\"skin\":" + "\"wall\""  +
           "}"
       }.mkString(",") + "]"
-    }
-    json += "}"
-
-    sendMessage(stream, json)
+    } + "}"
   }
-//
-//  def sendMovables(stream: java.io.OutputStream, movables: Set[Movable]) {
-//    var json = "{"
-//    json += {
-//      "\"maze\":" + "[" + maze.map {
-//        case ((column: Int, row: Int), skin: Space) =>
-//          "{" + "\"row\":" + row.toString + "," + "\"column\":" + column.toString + "," + "\"skin\":" + "\"space\""  + "}"
-//        case ((column: Int, row: Int), skin: Wall) =>
-//          "{" + "\"row\":" + row.toString + "," + "\"column\":" + column.toString + "," + "\"skin\":" + "\"wall\"" + "}"
-//      }.mkString(",") + "]"
-//    }
-//    json += "}"
-//  }
 
-  sendMaze(outputStream, game.maze)
-  //sendMovables(outputStream, game.maze)
-
-  while (true) {}
-
-  socket.close()
+  def movablesToJson(movables: Set[Movable]): String = {
+    "{" + {
+      "\"movables\":" + "[" + movables.map {
+        case (movable: Movable) =>
+          "{" + 
+            "\"direction\":"        + s""""${movable.direction}"""" + "," +
+            "\"directionRequest\":" + s""""${movable.directionRequest}"""" + "," + 
+            "\"speed\":"            + movable.speed + "," + 
+            "\"speedMs\":"          + movable.speedMs + "," + 
+            "\"width\":"            + movable.width + "," + 
+            "\"height\":"           + movable.height + "," + 
+            "\"id\":"               + s""""${movable.id}"""" + "," + 
+            "\"x\":"                + movable.x + "," +
+            "\"y\":"                + movable.y + 
+          "}"
+      }.mkString(",") + "]"
+    } + "}"
+  }
 }
 
--- pacman-web/main.coffee
+++ pacman-web/main.coffee
@@ -1,4 +1,4 @@
-
+# A simple game.
 class DrawableElement
 class Space extends DrawableElement
     constructor: (@consumableAvailable) ->
@@ -7,57 +7,100 @@ class Wall extends DrawableElement
 class Tile
     constructor: (@row, @column, @skin) ->
 
+class Movable
+    constructor: (movableData) ->
+        @x = movableData.x
+        @y = movableData.y
+
 getTileHeight = (maze, ctx) ->
     tilesY =
-        1 + maze.map (tile)-> tile.row
+        maze.map (tile) -> tile.row
             .reduce (rowCurrent, rowNext) ->
-                console.log(rowCurrent, rowNext)
                 if rowNext > rowCurrent then rowNext else rowCurrent
-
-    ctx.canvas.height / tilesY
+            , 0
+    ctx.canvas.height / (tilesY + 1)
 
 getTileWidth = (maze, ctx) ->
     tilesX =
-        1 + maze.map (tile)-> tile.column
+        maze.map (tile) -> tile.column
             .reduce (columnCurrent, columnNext) ->
                 if columnNext > columnCurrent then columnNext else columnCurrent
+            , 0
+    ctx.canvas.width / (tilesX + 1)
 
-    ctx.canvas.width / tilesX
-
-
-drawPellet = (tile, maze, ctx) ->
+drawPellets = (maze, ctx) ->
     tileWidth = getTileWidth(maze, ctx)
     tileHeight = getTileHeight(maze, ctx)
-    
-    ctx.fillStyle = "yellow"
-    ctx.arc(tile.column * tileWidth + tileWidth/2, tile.row * tileHeight + tileHeight/2, 5, 2 * Math.PI)
+
+    maze.forEach (tile) ->
+        if tile.skin.consumableAvailable
+            ctx.beginPath()
+            ctx.fillStyle = "yellow"
+            ctx.arc(tile.column * tileWidth + tileWidth/2, tile.row * tileHeight + tileHeight/2, 3, 0, 2 * Math.PI)
+            ctx.fill()
 
 drawMaze = (maze, ctx) ->
     tileWidth = getTileWidth(maze, ctx)
     tileHeight = getTileHeight(maze, ctx)
 
     maze.forEach (tile) ->
+        ctx.beginPath()
         ctx.fillStyle = 
             if tile.skin instanceof Space
-                if tile.consumableAvailable then drawPellet tile, maze, ctx
                 "blue"
             else if tile.skin instanceof Wall
                 "black"
         ctx.fillRect(tile.column * tileWidth, tile.row * tileHeight, tileWidth, tileHeight)
+        ctx.stroke()
+        
+drawMovables = (movables, maze, ctx) ->
+    tileWidth = getTileWidth(maze, ctx)
 
-    ctx.stroke()
+    movables.forEach (movable) ->
+        ctx.beginPath()
+        ctx.fillStyle = "yellow"
+        ctx.arc(tileWidth * movable.x, tileWidth * movable.y, 10, 0, 2 * Math.PI)
+        ctx.fill()
 
+class KeyCodes
+    @w = 119
+    @a = 97
+    @s = 115
+    @d = 100
 
+    @Up = 38
+    @Down = 40
+    @Left = 37
+    @Right = 39
 
 init = ->
     canvas = document.getElementById('game-canvas')
     context = canvas.getContext('2d')
-
+    
     socket = new WebSocket "ws://localhost:8000"
+
+    maze = []
+    movables = []
+
+
+    openingSong = new Audio '../sounds/opening-song.mp3'
+    openingSong.play()
+   
+    window.addEventListener "keydown", (ev) ->
+        console.log "player pressed #{ev.keyCode}"
+
+        socket.send switch ev.keyCode
+            when KeyCodes.w     then "North"
+            when KeyCodes.Up    then "North"
+            when KeyCodes.a     then "West"
+            when KeyCodes.Left  then "West"
+            when KeyCodes.s     then "South"
+            when KeyCodes.Down  then "South"
+            when KeyCodes.d     then "East"
+            when KeyCodes.Right then "East"
+
     socket.onmessage = (message) ->
         json = JSON.parse message.data
-
-        console.log(json)
 
         unless json.maze is undefined
             maze = json.maze.map (tile) ->
@@ -66,9 +109,13 @@ init = ->
                 else if tile.skin is 'wall'
                     new Tile(tile.row, tile.column, new Wall())
 
+        unless json.movables is undefined
+            movables = json.movables.map (movableData) -> new Movable(movableData)
 
-
+        context.clearRect(0, 0, canvas.width, canvas.height)
 
         drawMaze(maze, context)
+        drawPellets(maze, context)
+        drawMovables(movables, maze, context)
 
 init()
